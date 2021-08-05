af642be03d3dc6c1282392125d455fdfcf4e4402
Weihsiao Lin <43899469+updown321@users.noreply.github.com>
2021-08-05 16:36:08 +0800

    Update interface.go

--- lib/go/api/interface.go
+++ lib/go/api/interface.go
@@ -19,15 +19,10 @@ package api
 import "github.com/apache/incubator-yunikorn-scheduler-interface/lib/go/si"
 
 type SchedulerAPI interface {
-    // Register a new RM, if it is a reconnect from previous RM, cleanup 
-	// all in-memory data and resync with RM. 
 	RegisterResourceManager(request *si.RegisterResourceManagerRequest, callback ResourceManagerCallback) (*si.RegisterResourceManagerResponse, error)
 
-	// Update Scheduler status (including node status update, allocation request 
-	// updates, etc. 
 	Update(request *si.UpdateRequest) error
 
-	// Notify scheduler to reload configuration and hot-refresh in-memory state based on configuration changes 
 	ReloadConfiguration(clusterID string) error
 }
 
